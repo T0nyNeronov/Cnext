36e852591ce06491aa449ccdf2ff86023acf84a3
Antony <ashout10@gmail.com>
2022-11-29 18:34:32 +0300

    На вход 3 числа и провряет может ли

---
+++ Semi6_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник со сторонами такой длины.
+// *Каждая сторона треугольника иеньше суммы двух других сторон.*
+
+Console.WriteLine("Ввеедите 1 число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввеедите 2 число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ввеедите 3 число ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (c<=a+b&&a<=c+b&&b<=a+c)
+{
+    Console.WriteLine("Данные числа могут быть сторонами треугольника");
+}
+else
+{
+    Console.WriteLine("Данные числа не могут быть сторонами треугольника");
+}
