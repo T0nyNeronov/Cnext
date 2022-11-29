2cfff10dfe9fe4f47213c7f619aef6cac3a18c4e
Antony <ashout10@gmail.com>
2022-11-29 17:56:07 +0300

    Из десятичного в двоичное

---
+++ Semi6_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+// 45 -> 101101
+// 3 -> 11 
+// 2 -> 10
+
+Console.Write(" Двоичное число = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+string result ="";
+while (n > 0)
+{
+    result=n%2+result;
+    n/=2;
+}
+Console.WriteLine(result);
